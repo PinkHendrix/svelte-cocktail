70748a8c9c544920e192e7ad43129ca738ee46c7
Rick Hausz <pinkhendrix1881@gmail.com>
2022-09-12 14:21:59 -0700

    Scaffolding started with styles.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="typescript">
+	export let drinkProp;
+
+	let drinkState = drinkProp;
+
+	const handleOnClick = async () => console.log('Click!');
+</script>
+
+<div class="wrapper">
+	<button on:click={handleOnClick}>Get New Drink</button>
+	<h2>Drink Name</h2>
+	<img class="drink-thumb" src="/" alt="drink-thumb" />
+	<p>Instructions</p>
+
+	<p>Ingredients</p>
+</div>
+
+<style>
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+
+	.drink-thumb {
+		width: 300px;
+		border-radius: 1rem;
+	}
+
+	p {
+		min-width: 500px;
+		text-align: center;
+	}
+
+	.ingredients {
+		margin: 2px 0;
+	}
+</style>
